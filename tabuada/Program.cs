a4984804b6cd194afb638a004c369d5e604f3363
Guilherme da Silva Oliveira <118921497+guilhermedasilva611@users.noreply.github.com>
2023-03-19 22:56:58 -0300

    Adicionar arquivos de projeto.

---
+++ tabuada/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Bem vindo a TABUADA---");
+
+            bool continua = true;
+
+            int dec = 1;
+
+            do
+            {
+
+                Console.Write("Digite o numero que deseja saber a tabuada:");
+                int numero = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    Console.WriteLine($"{numero}X{i} = {i * numero}");
+                }
+
+                do
+                {
+                    Console.WriteLine("Deseja efetuar outra tabuada? (1)-sim (2)-não");
+                    dec = int.Parse(Console.ReadLine());
+
+                    if (dec == 1)
+                        continua = true;
+                    else if (dec == 2)
+                        continua = false;
+
+                } while (dec != 1 && dec != 2);
+
+
+            } while (continua);
+
+            Console.Write("--Pressione ENTER para encerrar--");
+            Console.ReadLine();
+
+        }
+    }
+}
